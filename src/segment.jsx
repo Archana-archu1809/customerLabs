11a092a1564046fd32d5d3c79444c8b675745795
Archana-archu1809 <narchu2000@gmail.com>
2024-09-11 19:25:06 +0530

    changes in dropdown

--- src/segment.jsx
+++ src/segment.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import * as formik from "formik";
-
-import { Drawer } from "antd";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Drawer, Input, Select, Button } from "antd";
 import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Form, Button } from "react-bootstrap";
+const { Option } = Select;
 
 const Segment = () => {
-  const { Formik } = formik;
-
   const [open, setOpen] = useState(false);
-  const data = [
+
+  const options = [
     {
       id: 1,
       value: "first_name",
@@ -26,7 +26,10 @@ const Segment = () => {
     { id: 6, value: "city", label: "City" },
     { id: 7, value: "state", label: "State" },
   ];
+
   const [fields, setFields] = useState([]);
+  const [schema, setSchema] = useState("");
+  const [selected, setSelected] = useState([]);
 
   const showDrawer = () => {
     setOpen(true);
@@ -35,119 +38,136 @@ const Segment = () => {
   const handleAddField = () => {
     setFields([...fields, ""]);
   };
-
-  const handleRemoveField = (index) => {
+  const [segment, setSegment] = useState("");
+  const handleRemoveField = (index, field) => {
     const newFields = fields.filter((fields, i) => i !== index);
     setFields(newFields);
+    setSelected(selected.filter((data, i) => data !== field));
   };
 
-  const handleFieldChange = (index, event) => {
+  const handleFieldChange = (index, value) => {
     const newfields = [...fields];
-    newfields[index] = event.target.value;
+    newfields[index] = value;
     setFields(newfields);
-  };
 
+    setSelected((oldArray) => [...oldArray, value]);
+  };
   const onClose = () => {
     setOpen(false);
   };
 
-  const submit = (values, { resetForm }) => {
-    console.log(values.segment, values.schema, fields);
-   
-  };
+  const handleOptions = (value) => {
+    setSchema(value);
 
+    setSelected((oldArray) => [...oldArray, value]);
+  };
+  const submit = () => {
+    const data = {
+      segment_name: segment,
+      schema: [
+        {
+          schema: schema,
+          fields: fields,
+        },
+      ],
+    };
+    axios
+      .post("https://dummy.restapiexample.com/api/v1/create", data)
+      .then((response) => console.log("Created Successfully", response));
+
+    setSegment("");
+    setSchema("");
+    setOpen(false);
+    
+  };
+  const handleCancel = () => {
+    setOpen(false);
+    setSegment("");
+    setSchema("");
+  };
   return (
     <>
-      <Button onClick={showDrawer}>Save segment</Button>
-      <Drawer title="Saving segment" onClose={onClose} open={open}>
-        <Formik
-          onSubmit={submit}
-          initialValues={{
-            segment: "",
-            schema: "",
-          }}
-        >
-          {({ handleSubmit, handleChange, values, touched, errors }) => (
-            <Form noValidate onSubmit={handleSubmit}>
-              <Form.Group className="mb-3">
-                <Form.Label className="form-label">
-                  Enter the Name of the Segment
-                </Form.Label>
-                <Form.Control
-                  type="text"
-                  name="segment"
-                  placeholder="Name of the segment"
-                  required
-                  onChange={handleChange}
-                  value={values.segment}
-                ></Form.Control>
-              </Form.Group>
-
-              <Form.Group className="mb-3">
-                <Form.Select
-                  className="form-select"
-                  name="schema"
-                  placeholder="Add schema to segment"
-                  required
-                  onChange={handleChange}
-                  value={values.schema}
-                >
-                  <option value="">Select Pairname</option>
-
-                  <option value="first_name">First Name</option>
-                  <option value="last_name">Last Name</option>
-                  <option value="gender">Gender</option>
-                  <option value="age">Age</option>
-                  <option value="account_name">Account Name</option>
-                  <option value="city">City</option>
-                  <option value="state">State</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Button onClick={handleAddField}>
-                <PlusOutlined />
-                Add new schema
-              </Button>
-
-              {fields.map((field, index) => (
-                <Form.Group key={index} className="mb-3">
-                  <Form.Select
-                    className="form-select"
-                    placeholder="Add schema to segment"
-                    onChange={(e) => handleFieldChange(index, e)}
-                    value={field}
-                  >
-                    <option value=""></option>
-                 
-
-                    {data
-                      .filter((data) => data.value !== values.schema)
-
-                      .map((data) => {
-                        return (
-                          <option key={data.id} value={data.value}>
-                            {data.label}
-                          </option>
-                        );
-                      })}
-                  </Form.Select>
-
-                  <MinusCircleOutlined
-                    onClick={() => handleRemoveField(index)}
-                  ></MinusCircleOutlined>
-                </Form.Group>
-              ))}
-
-              <Button
-                className="btn btn-primary"
-                type="submit"
-                variant="primary"
+      <Button
+        onClick={showDrawer}
+        variant="outline-light"
+        style={{ color: "black" }}
+      >
+        Save segment
+      </Button>
+      <Drawer title="Saving Segment" onClose={onClose} open={open} width={500}>
+        <div className="mb-3">
+          <label>Enter the Name of the segment</label>
+          <Input
+            value={segment}
+            placeholder="Name of the segment"
+            onChange={(e) => setSegment(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <Select
+            placeholder="Add schema to segment"
+            onChange={(value) => handleOptions(value)}
+            value={schema}
+            style={{
+              width: 400,
+            }}
+            required
+          >
+            {options.map((data, i) => {
+              return (
+                <Option key={data.id} value={data.value}>
+                  {data.label}
+                </Option>
+              );
+            })}
+          </Select>
+        </div>
+        <div className="mb-3">
+          <Button onClick={handleAddField} type="link">
+            <PlusOutlined />
+            Add new schema
+          </Button>
+        </div>
+        {fields.map((field, index) => (
+          <>
+            <div className="mb-3">
+              <Select
+                placeholder="Add schema to segment"
+                value={field}
+                onChange={(value) => handleFieldChange(index, value)}
+                style={{
+                  width: 400,
+                }}
               >
-                Save the segment
-              </Button>
-            </Form>
-          )}
-        </Formik>
+                {options
+                  .filter((n) => !selected.includes(n.value))
+                  .map((data, i) => {
+                    return (
+                      <Option key={data.id} value={data.value}>
+                        {data.label}
+                      </Option>
+                    );
+                  })}
+              </Select>
+              <MinusCircleOutlined
+                onClick={() => handleRemoveField(index, field)}
+              ></MinusCircleOutlined>
+            </div>
+          </>
+        ))}
+        <Row>
+          <Col>
+            <Button type="primary" onClick={submit}>
+              Save the segment
+            </Button>
+          </Col>
+          <Col>
+            <Button type="link" danger onClick={handleCancel}>
+              Cancel
+            </Button>
+          </Col>
+        </Row>
       </Drawer>
     </>
   );
